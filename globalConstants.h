b097abb87398a9370b093f62a906c97643453c39
Tony Xue <xuezxbb@gmail.com>
2014-05-08 19:46:31 +0800

    add a bunch of constants for judgements

--- globalConstants.h
+++ globalConstants.h
@@ -3,5 +3,15 @@ Author: Tony Xue
 E-mail: xuezhaoxin@rdfz.cn
 The code is released under GUN LGPL v3 license, please see "LICENSE" for details.
 */
+
+#define lightSensorPort 0;// Port number for sensor detects the startup and shutdown call
+#define lightIntensityCriticalValue 512;
+
+#define blackLineSensorPort 1;//Port number for sensor detects the black line on the game board
+#define blackLineCriticalValue 512;
+
+/*
+Constants defined under this section DO NOT need to be changed or modified when the surronding environment of the game board has changed
+*/
 #define centerX 80;// define the value of x axis of the center of the screen
 #define centerY 60;// define the value of y axis of the center of the screen
