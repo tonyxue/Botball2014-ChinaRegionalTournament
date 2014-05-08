d7d52bb6b6279b60d1a0de43eb0bb354127ff0c9
Tony Xue <xuezxbb@gmail.com>
2014-05-08 18:04:45 +0800

    use header file to define global variables
    use #define

---
+++ globalVariables.h
@@ -0,0 +1,7 @@
+/*
+Author: Tony Xue
+E-mail: xuezhaoxin@rdfz.cn
+The code is released under GUN LGPL v3 license, please see "LICENSE" for details.
+*/
+#define centerX 80;// define the value of x axis of the center of the screen
+#define centerY 60;// define the value of y axis of the center of the screen
