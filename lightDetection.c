065921ba087b92a1aa18882bc4bb16ef62fcefae
Tony Xue <xuezxbb@gmail.com>
2014-05-08 19:48:49 +0800

    add author & license information

--- lightDetection.c
+++ lightDetection.c
@@ -1,3 +1,8 @@
+/*
+Author: Tony Xue
+E-mail: xuezhaoxin@rdfz.cn
+The code is released under GUN LGPL v3 license, please see "LICENSE" for details.
+*/
 static int lightDetection()
 {
 	if(analog10(lightSensorPort)<lightIntensityCritialValue) run=1; //Change the run indicator to TRUE when light intensity goes below the critical value
