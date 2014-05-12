63789aeb9f8a34bb6b9a1c5e489428486b796208
Tony Xue <xuezxbb@gmail.com>
2014-05-12 20:39:01 +0800

    change the comment

--- initializations.c
+++ initializations.c
@@ -14,6 +14,6 @@ static void cameraInit(int resLv)
 	if (resLv==1) camera_open(LOW_RES);// Start the camera and set the resolution to LOW
 	if (resLv==2) camera_open(MED_RES);// Start the camera and set the resolution to MEDIUM
 	if (resLv==3) camera_open(HIGH_RES);// Start the camera and set the resolution to HIGH
-	camera_update();// Fetech the latest data from camera | Initialization of data
+	camera_update();// Get the latest data from camera | Initialization of data
 	printf("Camera OK.\n");
 }
