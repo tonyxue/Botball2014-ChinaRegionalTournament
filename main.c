9a3f70fc461fa2b4f8ba090a9cb1ffeb90a490fb
Tony Xue <xuezxbb@gmail.com>
2014-05-12 20:40:32 +0800

    add the basic frame of getting the colored cube

--- main.c
+++ main.c
@@ -1,56 +1,134 @@
 /*
 Author: Tony Xue
 E-mail: xuezhaoxin@rdfz.cn
-The code is released under GUN LGPL v3 license, please see "LICENSE" for details.
+LICENSE: GUN LGPL v3 (please see "LICENSE" for details)
 */
 #include <stdio.h>
 #include "initializations.c"
 
-int run=0,stop=0;//indicator for whether the game has started and stopped
-int blackLineSensorPort=1,blackLineCriticalValue=512;
 int centerX=80;// define the value of x axis of the center of the screen
 int centerY=60;// define the value of y axis of the center of the screen
+int diffX,diffY;
+int giveUpValue=20; // after this many times of trying to get the reading, if no result,the robot will give up
 
-void lightDetection(int run, int stop)
+void lightDetection() // Light detection
 {
-	int lightSensorPort=0,lightIntensityCriticalValue=512;
-	if(analog10(lightSensorPort) < lightIntensityCriticalValue) run=1; //Change the run indicator to TRUE when light intensity goes below the critical value
-	if(analog10(lightSensorPort) < lightIntensityCriticalValue) stop=1;run=0; //Change the stop indicator to TRUE and run indicator to FALSE when light intensity goes above the critical value
+	int lightSensorPortNum=0,reading=1024,lightCriticalValue=512; //define Light Sensor port number, critical value and initialize the reading
+	while (reading>lightCriticalValue)
+	{
+		reading=analog10(lightSensorPortNum);
+	}
 }
-int blackLine()
+
+int blackLine() // Black line detection
 {
+	int blackLineSensorPort=1,blackLineCriticalValue=512; //define the port for black line sensor and the critical value
 	if(analog10(blackLineSensorPort)<blackLineCriticalValue) return 1;// Return TRUE when black color is detected
 	if(analog10(blackLineSensorPort)>blackLineCriticalValue) return 0;// Return FALSE when white color is detected
 }
 
-int xyDiff(int channel,int size)
+void xyDiff(int channel,int size)
 {
-	int x,y,diffX,diffY;
-	if (get_object_count(channel) == 0) printf ("No colored object found!\n");
+	int x,y,diffX,diffY,i=0;
+	while (get_object_count(channel) == 0)
+	{
+		i++;// use this variable to count the times that the sensor detects no object, for debug purpose
+		printf ("No colored object found! This is the &d time!\n",i);
+		if (i==giveUpValue) printf("%d times reached, giving up!", giveUpValue);break; // If nothing is detected after serval times, the robot will give up the task
+	}
 	printf("Found object!\n");
 	x=get_object_center(channel,size).x; // get the x axis value of the largest object
 	y=get_object_center(channel,size).y; // get the y axis value of the largest object
 	diffX=x-centerX;// calculate the differences on x axis
 	diffY=y-centerY;// calculate the differences on y axis
-	return diffX,diffY;
 }
+
+int getCubes(char color,int yellowChannel=1, int orangeChannel=0)
+{
+	int yellowCount=get_object_count(yellowChannel),orangeCount=get_object_count(orangeChannel),yellowSize=0,orangeSize=0;
+	int i=0,j=0;
+	if (color=="yellow") // for yellow cubes
+	{
+		while(yellowCount=0 && i<=giveUpValue)
+		{
+			msleep(500); // wait 0.5 second to avoid too frequent detections
+			i++;
+			yellowCount=get_object_count(yellowChannel);
+			if (i==giveUpValue) printf("No yellow object detected!\n");
+		}
+		
+		xyDiff(yellowChannel,yellowSize);
+		while (diffX!=0) //adjust the x axis
+		{
+			if (diffX > centerX)
+			{
+				// Turn LEFT
+			}
+			if (diffX < centerX)
+			{
+				// Turn RIGHT
+			}
+			xyDiff(yellowChannel,yellowSize);
+		}
+		while (diffY!=0) //adjust the y axis
+		{
+			if (diffY > centerY)
+			{
+				// Turn DOWN
+			}
+			if (diffY < centerY )
+			{
+				// Turn UP
+			}
+			xyDiff(yellowChannel,yellowSize);
+		}
+	}
+	if (color=="orange")// for orange cubes
+	{
+		while(orangeCount=0 && j<=giveUpValue)
+		{
+			msleep(500); // wait 0.5 second to avoid too frequent detections
+			j++;
+			orangeCount=get_object_count(orangeChannel);
+			if (i==giveUpValue) printf("No orange object detected!\n");
+		}
+		xyDiff(orangeChannel,orangeSize);
+		while (diffX!=0) //adjust the x axis
+		{
+			// ###Use the projection method to calculate the offset!!!###
+			if (diffX > centerX)
+			{
+				// Turn LEFT
+			}
+			if (diffX < centerX)
+			{
+				// Turn RIGHT
+			}
+			xyDiff(orangeChannel,orangeSize);
+		}
+		while (diffY!=0) //adjust the y axis
+		{
+			if (diffY > centerY)
+			{
+				// Turn DOWN
+			}
+			if (diffY < centerY )
+			{
+				// Turn UP
+			}
+			xyDiff(orangeChannel,orangeSize);
+		}
+	}
+
+
+}
+
 int main()
 {
-	int resLv=1,channel=0,size=0,stbInterval=2000;
-	while(1) // main loop, loop forever
-	{
-		lightDetection(run,stop);
-		if(run)
-		{
-			//**********put the tasks that need to be done here**********
-			cameraInit(resLv);
-			xyDiff(channel,size);
-			msleep(stbInterval);// Standby for a short period of time, avoiding too frequent updates
-			camera_update();
-			camera_close();
-		}
-		if(stop && !run) break;
-	}
+	int resLv=1,channel=0,size=0,stbyInterval=2000;
+	lightDetection();// wait for the startup light
+	//
+	
 	printf("Done!\n");
 	
 	return 0;
