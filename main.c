6bdef744fb575b61810dbc94d4cc9ff88e7d2531
Tony Xue <xuezxbb@gmail.com>
2014-05-13 21:04:38 +0800

    add more functions

--- main.c
+++ main.c
@@ -6,10 +6,13 @@ LICENSE: GUN LGPL v3 (please see "LICENSE" for details)
 #include <stdio.h>
 #include "initializations.c"
 
-int centerX=80;// define the value of x axis of the center of the screen
-int centerY=60;// define the value of y axis of the center of the screen
-int diffX,diffY;
+int centerX=80,centerY=60;// define the x and y axis value of the center of the screen
+int offsetX,offsetY;//offset value for the servo
 int giveUpValue=20; // after this many times of trying to get the reading, if no result,the robot will give up
+int offsetStatus=0;
+int yellowChannel=1,orangeChannel=0,yellowSize=0,orangeSize=0,orangeStripChannel,yellowStripChannel;
+int liftingServoPort, rotationMotorPort, catchingServoPort, rotationMotorVelocity;// add the port number for these motors and servos here
+int putUpHangerOffset;
 
 void lightDetection() // Light detection
 {
@@ -27,109 +30,90 @@ int blackLine() // Black line detection
 	if(analog10(blackLineSensorPort)>blackLineCriticalValue) return 0;// Return FALSE when white color is detected
 }
 
-void xyDiff(int channel,int size)
+void offset(int channel,int size)
 {
 	int x,y,diffX,diffY,i=0;
 	while (get_object_count(channel) == 0)
 	{
 		i++;// use this variable to count the times that the sensor detects no object, for debug purpose
 		printf ("No colored object found! This is the &d time!\n",i);
-		if (i==giveUpValue) printf("%d times reached, giving up!", giveUpValue);break; // If nothing is detected after serval times, the robot will give up the task
+		if (i==giveUpValue) printf("%d times reached, giving up!", giveUpValue);offsetStatus=-1;return -1; // If nothing is detected after serval times, the robot will give up the task
 	}
 	printf("Found object!\n");
 	x=get_object_center(channel,size).x; // get the x axis value of the largest object
 	y=get_object_center(channel,size).y; // get the y axis value of the largest object
 	diffX=x-centerX;// calculate the differences on x axis
 	diffY=y-centerY;// calculate the differences on y axis
+	offsetX=5*(diffX);// The algorithm is provided by the sample code, might be wrong, IDK
+	offsetY=5*(diffY);
 }
 
-int getCubes(char color,int yellowChannel=1, int orangeChannel=0)
+void run()
 {
-	int yellowCount=get_object_count(yellowChannel),orangeCount=get_object_count(orangeChannel),yellowSize=0,orangeSize=0;
-	int i=0,j=0;
-	if (color=="yellow") // for yellow cubes
-	{
-		while(yellowCount=0 && i<=giveUpValue)
-		{
-			msleep(500); // wait 0.5 second to avoid too frequent detections
-			i++;
-			yellowCount=get_object_count(yellowChannel);
-			if (i==giveUpValue) printf("No yellow object detected!\n");
-		}
-		
-		xyDiff(yellowChannel,yellowSize);
-		while (diffX!=0) //adjust the x axis
-		{
-			if (diffX > centerX)
-			{
-				// Turn LEFT
-			}
-			if (diffX < centerX)
-			{
-				// Turn RIGHT
-			}
-			xyDiff(yellowChannel,yellowSize);
-		}
-		while (diffY!=0) //adjust the y axis
-		{
-			if (diffY > centerY)
-			{
-				// Turn DOWN
-			}
-			if (diffY < centerY )
-			{
-				// Turn UP
-			}
-			xyDiff(yellowChannel,yellowSize);
-		}
-	}
-	if (color=="orange")// for orange cubes
-	{
-		while(orangeCount=0 && j<=giveUpValue)
-		{
-			msleep(500); // wait 0.5 second to avoid too frequent detections
-			j++;
-			orangeCount=get_object_count(orangeChannel);
-			if (i==giveUpValue) printf("No orange object detected!\n");
-		}
-		xyDiff(orangeChannel,orangeSize);
-		while (diffX!=0) //adjust the x axis
-		{
-			// ###Use the projection method to calculate the offset!!!###
-			if (diffX > centerX)
-			{
-				// Turn LEFT
-			}
-			if (diffX < centerX)
-			{
-				// Turn RIGHT
-			}
-			xyDiff(orangeChannel,orangeSize);
-		}
-		while (diffY!=0) //adjust the y axis
-		{
-			if (diffY > centerY)
-			{
-				// Turn DOWN
-			}
-			if (diffY < centerY )
-			{
-				// Turn UP
-			}
-			xyDiff(orangeChannel,orangeSize);
-		}
-	}
-
-
+	mrp(rotationMotorPort,rotationMotorVelocity,offsetX);
+	set_servo_position(liftingServoPort,offsetY);
 }
 
-int main()
+void getCubes(int colorChoice)
+{
+	if (colorChoice==1) // for getting the yellow cubes
+	{
+		offset(yellowChannel,yellowSize);
+		//run the servo and motor here
+		run();
+	}
+	if (colorChoice==0) // for getting the orange cubes
+	{
+		offset(orangeChannel,orangeSize);
+		//run the servo and motor here
+		run();
+	}
+//
+}
+void seekHangerStand()
+{
+	
+}
+void putHangers() //put the hangers on the PVC
+{
+	set_servo_position(liftingServoPort,putUpHangerOffset);
+}
+
+void placeCubes()
+{
+	int yMax,yCenter,diffY,offset;
+	offset(orangeStripChannel,0);//when close enough to the orange strip, calculate the offset
+	run();
+	yMax=get_object_bbox(orangeChannel,0).uly;
+	yCenter=get_object_center.y;
+	diffY=yMax-yCenter;
+	offset=5*diffY;
+	set_servo_position(liftingServoPort,offset);// cancel the offset on y axis
+	msleep(1000);// wait for the servo to reach the position
+	set_servo_position(catchingServoPort,2047);// release the cube
+}
+
+void main()
 {
 	int resLv=1,channel=0,size=0,stbyInterval=2000;
 	lightDetection();// wait for the startup light
 	//
+	//yijia function 
+	//zhua cube
+	//song cube
+	//yuanlufanhui
+	//zhua cube
+	//song cube
+	//yuanlufanhui
+	//zhua cube
+	//song cube
+	
+	
+	getCubes(0);//get the orange cubes
+	if (offsetStatus==-1) printf("get orange cubes failed!\n");
+	getCubes(1);//get the yellow cubes
+	if (offsetStatus==-1) printf("get yellow cubes failed!\n");
 	
 	printf("Done!\n");
-	
 	return 0;
 }
